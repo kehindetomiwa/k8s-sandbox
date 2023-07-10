6cf65d9ef0d097e58e53090c62a1c714527cfc34
kehindetomiwa <kehinde.tomiwa4@gmail.com>
2023-07-10 15:27:22 +0200

    fff

--- minikube-cmd/minkube-cmd.sh
+++ minikube-cmd/minkube-cmd.sh
@@ -16,3 +16,27 @@ minikube addons enable metrics-server
 minikube addons enable dashboard
 minikube addons list
 minikube dashboard 
+
+
+#setup users, role, role binding for nameapace
+
+#generate key (user is victoryhouse)
+openssl genrsa -out victoryhouse.key 2048
+openssl req -new -key victoryhouse.key -out victoryhouse.csr -subj "/CN=victoryhouse/O=learner"
+
+cat bob.csr | base64 | tr -d '\n','%' #save output in signin=request file 
+
+kubectl create -f signing-request.yaml
+kubectl get csr
+kubectl certificate approve victoryhouse-csr
+
+#Extract the approved certificate from the certificate signing request, decode it with base64 and save it as a certificate file. Then view the certificate in the newly created certificate file:
+kubectl get csr victoryhouse-csr -o jsonpath='{.status.certificate}' | base64 -d > victoryhouse.crt
+
+#Configure the kubectl client's configuration manifest with user bob's credentials by assigning his key and certificate: 
+
+ kubectl config set-credentials victoryhouse --client-certificate=victoryhouse.crt --client-key=victoryhouse.key
+#Create a new context entry in the kubectl client's configuration manifest for user bob, associated with the lfs158 namespace in the minikube cluster:
+ kubectl config set-context victoryhouse-context --cluster=minikube --namespace=lfs158 --user=victoryhouse
+
+
